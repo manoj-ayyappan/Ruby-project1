5aeb646f6fb312707c193388122b1d2fe977e97d
mayyapp <mayyapp@ncsu.edu>
2023-09-08 10:21:27 -0400

    Added the code template

---
+++ ruby_intro_fall23.rb
@@ -0,0 +1,30 @@
+# Part 1
+def two_sum?(a, n)
+  # ADD YOUR CODE HERE
+end
+
+def max_sub_array(a)
+  # ADD YOUR CODE HERE
+end
+
+def group_anagrams(a)
+  # ADD YOUR CODE HERE
+end
+
+# Part 2
+def brackets_match?(s)
+  # ADD YOUR CODE HERE
+end
+
+def remove_and_append_vowels(s)
+  # ADD YOUR CODE HERE
+end
+
+def highest_frequency_word(s)
+  # ADD YOUR CODE HERE
+end
+
+# Part 3
+class Book
+  # ADD YOUR CODE HERE
+end
